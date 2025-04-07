e210a84f27937a595ae1d51e2b3dfd5abd42c317
Gabriel Cioci <ciocigabi@gmail.com>
2025-04-07 17:08:48 +0300

    fix ts error

--- components/ExperienceSection/types.ts
+++ components/ExperienceSection/types.ts
@@ -11,7 +11,7 @@ export type ExperienceItemType = {
     end_date?: string;
     tech?: string[];
     industry?: string;
-    achievements?: string[];
+    achievements?: string[] | string;
     links?: ExperienceLinkProps[];
 }
 
